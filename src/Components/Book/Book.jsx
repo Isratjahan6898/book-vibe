38e610436d5b9b578d216d46bb5420fabaffd194
israt6898 <israt6898@gmail.com>
2024-03-26 20:44:27 +0600

    load data

---
+++ src/Components/Book/Book.jsx
@@ -0,0 +1,39 @@
+import { FaRegStar } from "react-icons/fa";
+
+const Book = ({book}) => {
+
+    const {image,tags,bookName, author, category, rating} = book;
+    const [first, second]= tags;
+    console.log(book);
+    return (
+        <div>
+            <div className="card w-[350px] bg-base-100 shadow-xl">
+  <figure className="px-10 pt-10">
+    <img src={image} alt="Shoes" className="rounded-xl w-[300px] h-[230px]" />
+  </figure>
+  <div className="mt-[20px]">
+       <button className="mr-[20px]- ml-[40px] bg-[#23BE0A0D] p-[10px] #23BE0A0D text-[#23BE0A]">{first}</button>
+       <button className="ml-[30px] bg-[#23BE0A0D] p-[10px] #23BE0A0D text-[#23BE0A]">{second}</button>
+  </div>
+  <div className="card-body ">
+    <h2 className="card-title">{bookName}</h2>
+    <p>by:{author}</p>
+    <hr className="border-dotted border-2"></hr>
+   <div className="flex justify-between">
+   <div>
+        <p>{category}</p>
+
+     </div>
+
+     <div>
+        <p className="flex items-center"><FaRegStar />{rating}</p>
+     </div>
+   </div>
+  </div>
+</div>
+            
+        </div>
+    );
+};
+
+export default Book;
